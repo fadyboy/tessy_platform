6905cb7cc30f6fafd010fd12069c9ab0d28c1496
Anthony Fadero <afadero@opusonesolutions.com>
2020-01-05 22:13:08 -0500

    Added login form template and login route

--- student-platform/studentapp/routes.py
+++ student-platform/studentapp/routes.py
@@ -1,7 +1,17 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from studentapp import app
+from studentapp.forms import LoginForm
 
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template("index.html", title="Home")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f"Login requested for {form.username.data}")
+        return redirect(url_for("login"))
+    return render_template("login.html", form=form, title="Log In")
