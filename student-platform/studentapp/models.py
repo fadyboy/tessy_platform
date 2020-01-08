7167c9236a8a7233097f0a854ea2fc538722a5bd
Anthony Fadero <afadero@opusonesolutions.com>
2020-01-07 22:09:22 -0500

    Changed db name to Users

--- student-platform/studentapp/models.py
+++ student-platform/studentapp/models.py
@@ -1,14 +1,13 @@
 from studentapp import db
 
 
-class AdminUsers(db.Model):
-    __tablename__ = "AdminUsers"
+class Users(db.Model):
+    __tablename__ = "Users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     role = db.Column(db.String(16))
 
-
     def __repr__(self):
-        return f"Admin User: {self.username}"
+        return f"User: {self.username}"
