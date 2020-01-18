d040de1fd206ecb5a0d6c4723f05c3a5687ea67a
Anthony Fadero <afadero@opusonesolutions.com>
2020-01-18 18:29:50 -0500

    Added add user form, template and route to create users for platform

--- student-platform/studentapp/forms.py
+++ student-platform/studentapp/forms.py
@@ -1,9 +1,46 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms_sqlalchemy.fields import QuerySelectField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from studentapp.models import Role, User
 
 
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Log In")
+
+# Helper function for the query_factory param
+def roles_query():
+    return Role.query
+
+# Helper function to resolve Value error for QuerySelectField
+def get_pk(obj):
+    return str(obj)
+
+
+class AddUserForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    password2 = PasswordField("Repeat Password", validators=[DataRequired(), EqualTo("password")])
+    roles = QuerySelectField(query_factory=roles_query, allow_blank=True, get_label="name", get_pk=get_pk,
+                             validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError("Username already exists! Please use another username")
+
+    def validate_email(self, email):
+        user_email = User.query.filter_by(email=email.data).first()
+        if user_email is not None:
+            raise ValidationError("Email already exists! Please enter another email")
+
+
+
+
+
+
+
--- student-platform/studentapp/routes.py
+++ student-platform/studentapp/routes.py
@@ -1,14 +1,16 @@
-from flask import render_template, flash, redirect, url_for
-from studentapp import app
-from studentapp.forms import LoginForm
+from flask import render_template, flash, redirect, url_for, request
+from werkzeug.urls import url_parse
+from studentapp import app, db
+from studentapp.forms import LoginForm, AddUserForm
 from studentapp.models import User
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 
 
 @app.route("/")
 @app.route("/index")
+@login_required
 def index():
-    return render_template("index.html", title="Home", user=None)
+    return render_template("index.html", title="Home")
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -18,12 +20,15 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
-        if user is not None and user.check_password(form.password.data):
-            # load user
-            login_user(user)
-            return redirect(url_for("index"))
-        flash(f"Invalid username or password provided")
-        return redirect(url_for("login"))
+        if user is None or not user.check_password(form.password.data):
+            flash(f"Invalid username or password provided")
+            return redirect(url_for("login"))
+        # load user
+        login_user(user)
+        next_page = request.args.get("next")
+        if not next_page or url_parse(next_page).netloc == "":
+            next_page = url_for("index")
+        return redirect(next_page)
     return render_template("login.html", form=form, title="Log In")
 
 
@@ -34,5 +39,21 @@ def logout():
 
 
 @app.route("/admin")
+@login_required
 def admin():
     return render_template("admin.html", title="Admin")
+
+
+@app.route("/add_user", methods=["GET", "POST"])
+@login_required
+def add_user():
+    form = AddUserForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password_hash(form.password.data)
+        user.role = form.roles.data
+        db.session.add(user)
+        db.session.commit()
+        flash(f"{user.username} user added")
+        return redirect(url_for("add_user")) # redirect used to clear form
+    return render_template("add_user.html", title="Add User", form=form)
