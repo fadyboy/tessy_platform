1f9b88130c097238811fcd93d463e8b01bb58c85
Anthony Fadero <afadero@opusonesolutions.com>
2020-01-29 21:45:29 -0500

    missed commits for config.py

--- student-platform/config.py
+++ student-platform/config.py
@@ -5,6 +5,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "my-super-duper-secret-key"
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or f"sqlite:///{os.path.join(basedir, 'studentapp.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # configure how many users, students, staff displayed on page
+    MAX_USERS_PER_PAGE = 3
