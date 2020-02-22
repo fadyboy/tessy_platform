2b7f7a0c49e9e59cde9e72d6525640b3c13bc37b
Anthony Fadero <afadero@opusonesolutions.com>
2020-02-22 15:55:29 -0500

    added new models to the shell context processor

--- student-platform/student_platform.py
+++ student-platform/student_platform.py
@@ -1,8 +1,8 @@
 from studentapp import app, db
-from studentapp.models import User, Staff, Student, Classroom, Role, Subject, Session
+from studentapp.models import User, Staff, Student, Classroom, Role, Subject, Sessions, ScoreType
 
 
 @app.shell_context_processor
 def make_shell_context():
     return {"db": db, "User": User, "Staff": Staff, "Student": Student, "Classroom": Classroom, "Role": Role,
-            "Subject": Subject, "Session": Session}
+            "Subject": Subject, "Session": Sessions, "Score": ScoreType}
