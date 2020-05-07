fefb6d48fb539bafcae9460a11510463631c0b55
Anthony Fadero <afadero@opusonesolutions.com>
2020-05-06 22:28:28 -0400

    Reformatted file to flake8 std

--- student-platform/student_platform.py
+++ student-platform/student_platform.py
@@ -1,8 +1,18 @@
 from studentapp import app, db
-from studentapp.models import User, Staff, Student, Classroom, Role, Subject, Sessions, StudentResults
+from studentapp.models import User, Staff, Student, Classroom, Role, Subject,\
+    Sessions, StudentResults
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {"db": db, "User": User, "Staff": Staff, "Student": Student, "Classroom": Classroom, "Role": Role,
-            "Subject": Subject, "Session": Sessions, "Results": StudentResults}
+    return {
+        "db": db,
+        "User": User,
+        "Staff": Staff,
+        "Student": Student,
+        "Classroom": Classroom,
+        "Role": Role,
+        "Subject": Subject,
+        "Session": Sessions,
+        "Results": StudentResults
+        }
