f31e1200a30a7c628b3152f9b0fbd26c2442f26d
Anthony Fadero <afadero@opusonesolutions.com>
2020-05-13 23:28:06 -0400

    Moved app.config statement from with block

--- student-platform/studentapp/__init__.py
+++ student-platform/studentapp/__init__.py
@@ -18,9 +18,9 @@ mail = Mail()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
+    app.config.from_object(config_class)
     # initialize extensions within the app context
     with app.app_context():
-        app.config.from_object(config_class)
         db.init_app(app)
         migrate.init_app(app, db)
         login.init_app(app)
