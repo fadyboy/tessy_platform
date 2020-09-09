608d237791a8d8790bab2ffaf33aa6467bfa94d1
Anthony Fadero <aofadero@gmail.com>
2020-09-08 21:40:27 -0400

    Updated dockerfile and boot shell script

--- student-platform/boot.sh
+++ student-platform/boot.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
 source venv/bin/activate
-flask db upgrade
-exec gunicorn -b :5000 --access-logfile - --error-logfile - studentapp:app
+while true; do
+    flask db upgrade
+    if [[ "$?" == "0" ]]; then
+        break
+    fi
+    echo "Upgrage command failed, retrying in 5 sec..."
+    sleep 5
+done
+exec gunicorn -b :5000 --access-logfile - --error-logfile - student_platform:app
