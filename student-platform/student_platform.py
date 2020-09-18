082a89d410158de2dcd37c2fd5ecf221a5c555a2
Anthony Fadero <aofadero@gmail.com>
2020-09-17 22:37:30 -0400

    Added docker-compose.yml file and cli command to seed admin user

--- student-platform/student_platform.py
+++ student-platform/student_platform.py
@@ -1,8 +1,10 @@
 from studentapp import create_app, db
 from studentapp.models import (User, Staff, Student, Classroom, Role, Subject,
                                Sessions, StudentResults)
+from flask.cli import FlaskGroup
 
 app = create_app()
+cli = FlaskGroup(create_app=create_app)
 
 
 @app.shell_context_processor
@@ -18,3 +20,20 @@ def make_shell_context():
         "Session": Sessions,
         "Results": StudentResults
     }
+
+
+@cli.command("create_admin_user")
+def create_admin_user():
+    role1 = Role(name="Admin")
+    role2 = Role(name="User")
+    db.session.add_all([role1, role2])
+    db.session.commit()
+
+    admin = User(username="admin", email="admin@example.com", role=role1)
+    admin.set_password_hash("secret_password")
+    db.session.add(admin)
+    db.session.commit()
+
+
+if __name__ == "__main__":
+    cli()
